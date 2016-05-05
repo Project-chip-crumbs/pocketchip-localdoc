4bd92f26fb4926f6cb997d52b0f42aea895b1018
computermouth <computermouth@crunchbangplusplus.org>
2016-05-05 15:06:40 -0700

    remove python script, add sed and perl replaces to preparation script

--- prepare.sh
+++ prepare.sh
@@ -4,10 +4,14 @@ pushd pocketchip-docs
 git checkout gh-pages
 popd
 
-mv pocketchip-docs/* pocketchip-localdoc/pocketchip-localdoc/
-pushd pocketchip-localdoc/pocketchip-localdoc
+cp -R pocketchip-docs/* pocketchip-localdoc/
+pushd pocketchip-localdoc/
 for file in ./images/*; do convert $file -resize 400 $file; done
+
+sed -i -e 's/<script.*script>//' index.html
+perl -i -pe 'BEGIN{undef $/;} s/<a href="#" id="nav-button".*page-wrapper">/    <div class="page-wrapper">/smg' index.html
+
 popd
 
-./bs.py > pocketchip-localdoc/new-index.html
-mv pocketchip-localdoc/new-index.html pocketchip-localdoc/index.html
+#./bs.py > pocketchip-localdoc/new-index.html
+#mv pocketchip-localdoc/new-index.html pocketchip-localdoc/index.html
