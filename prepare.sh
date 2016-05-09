3937fc22ac6f7ab0d84742ab32303cd9e5f00ec8
computermouth <computermouth@crunchbangplusplus.org>
2016-05-09 15:17:58 -0700

    Remove links

--- prepare.sh
+++ prepare.sh
@@ -11,6 +11,8 @@ for file in ./images/*; do convert $file -resize 400 $file; done
 
 sed -i -e 's/<script.*script>//' index.html
 perl -i -pe 'BEGIN{undef $/;} s/<a href="#" id="nav-button".*page-wrapper">/    <div class="page-wrapper">/smg' index.html
+sed -i -e 's/<a href.*>//' index.html
+sed -i -e 's/<\/a>//' index.html
 
 popd
 
